727e2d83202eaf60aa31e94ac84bd2e1823a98d7
Richard Nguyen <cuongnt1987@live.com>
2015-03-26 15:27:23 +0700

    Add Constant

---
+++ docroot/WEB-INF/src/com/cmcti/cmts/portlet/util/CableModemConstants.java
@@ -0,0 +1,11 @@
+package com.cmcti.cmts.portlet.util;
+
+public interface CableModemConstants {
+	public static final int STATUS_OTHER = 1;
+	public static final int STATUS_RANGING = 2;
+	public static final int STATUS_RANGING_ABORTED = 3;
+	public static final int STATUS_RANGING_COMPLETE = 4;
+	public static final int STATUS_IP_COMPLETE = 5;
+	public static final int STATUS_REGISTRATION_COMPLETE = 6;
+	public static final int STATUS_ACCESS_DENIED = 7;
+}
